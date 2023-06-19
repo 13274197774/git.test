dafeed2f856f7e0e7fa346cea6b0893f0a5f6f6e
‘zhandchengjian’ <466569420@qq.com>
2023-06-19 16:35:11 +0800

    commit gitee-master update 1

--- src/main/java/com/zhangchenjian/git/gitTest.java
+++ src/main/java/com/zhangchenjian/git/gitTest.java
@@ -5,10 +5,6 @@ public class gitTest {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
-        System.out.println("dev4");
-        System.out.println("dev5");
-        System.out.println("master  5");
-        System.out.println("push  git");
-        System.out.println("我添加一条数据 拉取测试");
+        
     }
 }
